3863069ef96dcc1a13f0c415df2d3538a276d8d4
mofashy <mofashy@163.com>
2019-04-02 16:08:08 +0800

    Initial Commit

---
+++ ZDaily/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ZDaily
+//
+//  Created by 沈永聪 on 2019/4/2.
+//  Copyright © 2019 mofashy.com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
